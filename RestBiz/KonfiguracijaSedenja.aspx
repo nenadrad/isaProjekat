e08a23f07f8459ce287ca4358a484459291f8705
Nenad Rad <nenad.rad933@live.com>
2016-02-20 17:17:18 +0100

    konfiguracija sedenja

---
+++ RestBiz/KonfiguracijaSedenja.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="KonfiguracijaSedenja.aspx.cs" Inherits="RestBiz.KonfiguracijaSedenja" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <script type="text/javascript" src="Scripts/konfiguracijaSedenja.js"></script>
+
+    <div class="content-subhead">
+        <asp:Literal ID="ProfileHeader" runat="server"></asp:Literal>
+    </div>
+
+</asp:Content>
---
+++ RestBiz/KonfiguracijaSedenja.aspx.cs
@@ -0,0 +1,43 @@
+﻿using RestBiz.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RestBiz
+{
+    public partial class KonfiguracijaSedenja : System.Web.UI.Page
+    {
+
+        public int IdRestorana
+        {
+            get
+            {
+                return Convert.ToInt32(Request.QueryString["idRest"]);
+            }
+            set
+            {
+
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                string nazivRestorana;
+                using (var ctx = new RestBizContext())
+                {
+                    nazivRestorana = ctx.Restorani.Find(IdRestorana).Naziv;
+                }
+
+
+                ProfileHeader.Text = "<h2>" + nazivRestorana + " - konfiguracija sedenja</h2>";
+            }
+
+        }
+    }
+}
